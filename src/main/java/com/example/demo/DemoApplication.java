19603bef84e9bc9ef12853897b93d9332395c940
Vincent Maseko <vincent@sov.tech>
2018-10-18 12:07:50 +0200

    added basepackage classes for the app to search for the controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.controller.DemoController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = DemoController.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
