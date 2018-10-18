b5daac1282353aeeb01a2bede310aadcc3ee14f5
Kwasi Gyasi-Agyei <kwasi@edged.agency>
2018-10-18 14:17:12 +0200

    Updated project to match working version on github

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@Controller
-@RequestMapping("/demo")
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+//@RequestMapping("/demo")
+@RestController("/demo")
 public class DemoController {
 
     @ResponseBody
