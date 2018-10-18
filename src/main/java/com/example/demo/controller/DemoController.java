ce527ef7c1210dba8b702f0969a713a5a9d07e42
Vincent Maseko <vincent@sov.tech>
2018-10-18 12:25:41 +0200

    updatee the Controller annotation to be RestController

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,18 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
-@Controller
+@RestController
 @RequestMapping("/demo")
 public class DemoController {
 
-    @ResponseBody
-    @RequestMapping(method = GET, produces = "application/json")
-    public String demo() {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
         return "{\"hello\":\"world\"}";
     }
 }
